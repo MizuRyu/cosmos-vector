f91d7d65b764f5a564eccfa99086fe49e710a2e7
MizuRyu <02rlmdev@gmail.com>
2024-05-27 23:22:47 +0900

    add: aoai embedding model connect

--- src/config.py
+++ src/config.py
@@ -2,6 +2,7 @@ from dataclasses import dataclass
 import os
 from dotenv import load_dotenv
 
+# 環境変数読み込み
 load_dotenv()
 
 @dataclass
@@ -11,10 +12,9 @@ class Config:
     COSMOS_CONNECTION_STRING: str = os.getenv('AZURE_COSMOS_CONNECTION_STRING')
     COSMOS_DATABASE_NAME: str = os.getenv('AZURE_COSMOS_DATABASE_NAME')
     COSMOS_CONTAINER_NAME: str = os.getenv('AZURE_COSMOS_CONTAINER_NAME')
+    AZURE_OPENAI_ENDPOINT: str = os.getenv('AZURE_OPENAI_ENDPOINT')
+    AZURE_OPENAI_API_KEY: str = os.getenv('AZURE_OPENAI_API_KEY')
+    AZURE_OPENAI_API_VERSION: str = os.getenv('AZURE_OPENAI_API_VERSION')
+    AZURE_OPENAI_MODEL_NAME: str = os.getenv('AZURE_OPENAI_MODEL_NAME')
 
 config = Config()
-print("####Config values:")
-print(config.COSMOS_ENDPOINT)
-print(config.COSMOS_KEY)
-print(config.COSMOS_CONNECTION_STRING)
-print(config.COSMOS_DATABASE_NAME)
